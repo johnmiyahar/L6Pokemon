65b992b925098ea9c4b84d01ea33524af5f6dbb0
johnmiyahara <johnmiyahar@gmail.com>
2024-11-27 00:43:24 +0800

    Final Commit

---
+++ Data.js
@@ -0,0 +1,24 @@
+const datasource = [
+    {
+        title: 'GRASS',
+        bgColor: 'lightgreen',
+        iconColor: 'darkgreen',
+        iconName: 'leaf',
+        data: [
+            { key: 'Bulbasaur', itemNo: '1' },
+            { key: 'Butterfree', itemNo: '12' },
+        ],
+    },
+    {
+        title: 'WATER',
+        bgColor: 'skyblue',
+        iconColor: 'darkblue',
+        iconName: 'water',
+        data: [
+            { key: 'Squirtle', itemNo: '7' },
+            { key: 'Psyduck', itemNo: '54' },
+        ],
+    },
+];
+
+export { datasource };
